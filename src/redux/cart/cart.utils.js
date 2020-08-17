f6347d435a39f1665c40e6a44e47a5a3b369201f
Shubham Thakur <shubhamthakur.90@gmail.com>
2020-08-17 08:49:22 +0530

    added redux-persist

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -24,7 +24,7 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
     }
 
     return cartItems.map(cartItem => 
-        cartItem.id === cartItemToRemove
+        cartItem.id === cartItemToRemove.id
             ? {...cartItem, quantity: cartItem.quantity - 1}
             : cartItem
     );
